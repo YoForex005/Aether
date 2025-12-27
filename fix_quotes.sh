e16bec39c33cccdbbfcf0576e366afc1d83218ca
iamangiri <amangiri0381@gmail.com>
2025-12-27 09:40:33 +0530

    delete dummy file

--- fix_quotes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Fixing mismatched quotes..."
-
-# Fix all files with mismatched quotes
-find src -type f \( -name "*.tsx" -o -name "*.ts" \) | while read file; do
-  sed -i '' "s|from \"\([^']*\)'|from '\1'|g" "$file"
-done
-
-echo "Done!"
